d91a74b47760fb734977ebc583efa287b9c41fe7
userMary <mariyagmariya@yandex.ru>
2024-09-05 08:46:56 +0300

    My resume is in Angular

---
+++ src/app/resume/resume.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-resume',
+  standalone: true,
+  imports: [],
+  templateUrl: './resume.component.html',
+  styleUrl: './resume.component.css'
+})
+export class ResumeComponent {
+
+}
